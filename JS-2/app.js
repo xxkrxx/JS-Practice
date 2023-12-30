7d9a2fcfb1aa32d9e8596cc7235a007d80bff300
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2023-12-30 17:39:59 +0900

    ページネーション編集

---
+++ JS-2/app.js
@@ -0,0 +1,95 @@
+// 変数の書き方
+// let hello = 'Hello World
+// alert(hello);
+
+
+// 整数を代入する
+// let int1 = 1;
+
+// // 負数を代入する
+// let int2 = -10;
+
+// // 小数点を代入する
+// let float1 = 3.14;
+
+// // 文字列を代入する
+// let str1 = 'JavaScriptを覚えよう';
+
+
+// 四則演算の書き方
+// // 足し算
+// alert(4 + 3);
+
+// // 引き算
+// alert(8 - 5);
+
+// // 掛け算
+// alert(2 * 6);
+
+// // 割り算
+// alert(10 / 2);
+
+// alert('Hello' + 'World');
+
+
+// 文字列を結合する
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1 + str2);
+
+
+
+// 条件分岐
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い')
+// }
+
+
+// 繰り返し処理 while文
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max +'を超えるのに必要だった回数は' + count + '回です');
+
+
+// do...while文
+// do{
+//   最低1回は行われる処理
+// }while(条件式);
+
+
+// for文
+
+// let i;
+// let num = 0;
+// // 数字を「1」から「10」まで足し算
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+let int1 = 4 + 3;
+alert("足し算: 4 + 3 = " + int1);
+
+let int2 = 8 - 5;
+alert("引き算: 8 - 5 =" + int2);
+
+let int3 = 2 * 6;
+alert("掛け算: 2 * 6 =" + int3);
+
+let int4 = 10 /2;
+alert("割り算: 10 / 2 =" + int4);
